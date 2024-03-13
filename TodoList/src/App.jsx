a4704a71470af79e459044e6f3bcf9445d70f6d3
menna-adel-10 <menna10_adel@hotmail.com>
2024-03-13 16:54:59 +0200

    modified:   TodoList/src/App.jsx

--- TodoList/src/App.jsx
+++ TodoList/src/App.jsx
@@ -4,10 +4,10 @@ import './App.css'
 
 function App() {
 const [items,setItems] = useState([
-        { text: "Learn JavaScript", done: false },
-        { text: "Learn React", done: false },
-        { text: "Play around in JSFiddle", done: true },
-        { text: "Build something awesome", done: true }
+    { id: 1, text: "Learn JavaScript", done: false },
+    { id: 2, text: "Learn React", done: false },
+    { id: 3, text: "Play around in JSFiddle", done: true },
+    { id: 4, text: "Build something awesome", done: true }
       ]);
       
        const handleClick = (index) => {
